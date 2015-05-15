90dd13c32141742b0e69c58ba5509878baa3385c
jocelyn alsdorf & jocelyn <student@epicodus.com>
2015-05-15 11:29:18 -0700

    add route handlers for adding, deleting, editing and listing a band; adds views for corresponding paths

--- app.rb
+++ app.rb
@@ -9,3 +9,39 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/bands') do
+  @bands = Band.all()
+  erb(:bands)
+end
+
+post('/bands') do
+  name = params.fetch('name')
+  @new_band = Band.new({:name => name})
+
+  if @new_band.save()
+    erb(:success)
+  else
+    erb(:errors)
+  end
+end
+
+get('/bands/:id/edit') do
+  @band = Band.find(params.fetch('id').to_i)
+  erb(:edit_band)
+end
+
+patch('/bands/:id') do
+  name = params.fetch('name')
+  @band = Band.find(params.fetch('id').to_i)
+  @band.update({:name => name})
+  @bands = Band.all()
+  erb(:bands)
+end
+
+delete('/bands/:id') do
+  @band = Band.find(params.fetch('id').to_i)
+  @band.delete()
+  @bands = Band.all()
+  erb(:bands)
+end
